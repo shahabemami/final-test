eaf88f30cf1ec74045c4987ccb15537cc6c5a7ad
shahab emami <shahbemami95@gmail.com>
2020-06-20 19:34:53 +0430

    Add build folder.

---
+++ build/precache-manifest.495f904bb9a44701db9ad030f2e2b145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ef15dd49e51e7c82aad9dc7f3c7016",
+    "url": "/final-test/index.html"
+  },
+  {
+    "revision": "f40b874860d2df491c15",
+    "url": "/final-test/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "e5bcbf2d30f79acb7867",
+    "url": "/final-test/static/css/main.eda6aebc.chunk.css"
+  },
+  {
+    "revision": "f40b874860d2df491c15",
+    "url": "/final-test/static/js/2.f5e1d1ce.chunk.js"
+  },
+  {
+    "revision": "3b8464eda45e1c213d7f6886a962bc0b",
+    "url": "/final-test/static/js/2.f5e1d1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bcbf2d30f79acb7867",
+    "url": "/final-test/static/js/main.7c17e8b5.chunk.js"
+  },
+  {
+    "revision": "243808457ee75c4a450e",
+    "url": "/final-test/static/js/runtime-main.7ba74528.js"
+  }
+]);
